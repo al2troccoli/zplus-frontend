6005964418e8b17e99a96f1863dc55cac7e5da56
Aldo Troccoli <aatroccoli@minsait.com>
2022-10-10 19:05:41 +0200

    English translations are completed

--- lang/en-US.js
+++ lang/en-US.js
@@ -4,31 +4,71 @@ export default {
   error: {
     title: 'Error',
     wrong: 'Something went wrong!',
-    backToStart: 'Back to start page'
+    backToStart: 'Back to start page',
+    noAnimalInfo: 'There is no animal information to display.',
   },
   actions: {
-    connect: 'Connect',
-    disconnect: 'Disconnect',
     refresh: 'Refresh',
     applyChanges: 'Apply changes',
     cancel: 'Cancel',
     close: 'Close',
-    grade: 'Grade',
-    filter: 'Filter',
     exit: 'Exit',
-    start: 'Go to start'
+    back: 'Back',
+    start: 'Start'
   },
   commons: {
-    id: 'id',
-    name: 'Nombre',
-    type: 'Tipo',
-    breed: 'Raza',
-    gender: 'Género',
-    vaccinated: 'Vacunado',
-    lastVisit: 'Última visita',
-    lastUpdate: 'Última actualización',
+    id: 'Id',
+    name: 'Name',
+    type: 'Type',
+    breed: 'Breed',
+    gender: 'Gender',
+    vaccinated: 'Vaccinated',
+    lastVisit: 'Last visit',
+    lastUpdate: 'Last update',
+    actions: 'Actions',
+    animalsType: 'Animal type',
+    required: 'This field is required',
+    attention: 'Attention!'
   },
-  login: {
-    loading: 'Starting the application...',
+  intro: {
+    title: 'Animals registry',
+    text: 'This app is a code test for Zooplus company.'
+  },
+  animalsList: {
+    pageName: 'Animals list',
+    registeredAnimals: 'Registered animals',
+    createAnimal: 'Create a new animal',
+    loadingAnimals: 'Loading animals...',
+    editAnimal: 'Edit animal',
+    deleteAnimal: 'Delete animal',
+    deleteAnimalConfirmationText: 'Confirm that you want to remove the {animalType} named: {animalName}.',
+    deleteAnimalType: 'Delete {animalType}',
+    deletingAnimal: 'Deleting animal...',
+    deletedSuccessfully: 'The animal has been successfully deleted',
+  },
+  animalDetails: {
+    pageName: 'Animal details',
+    editAnimal: 'Modify animal',
+    editingAnimal: 'Modifying animal..',
+    editedSuccessfully: 'The animal has been modified successfully'
+  },
+  animalCreation: {
+    pageName: 'Creation of a new animal',
+    createAnimal: 'Create animal',
+    creatingAnimal: 'Creating animal...',
+    createdSuccessfully: 'The animal has been created successfully'
+  },
+  animalsTypes: {
+    cat: 'Cat',
+    dog: 'Dog',
+    bird: 'Bird',
+    fish: 'Fish',
+    rodent: 'Rodent',
+    unknown: 'Unknown'
+  },
+  animalsGender: {
+    male: 'Male',
+    female: 'Female',
+    unknown: 'Unknown'
   }
 }
